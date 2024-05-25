cee27f826724cc0ed4b24ab23873e911ec94d869
Ramakrishnan.R <147907676+RamakrishnanRamanathan@users.noreply.github.com>
2024-05-25 22:18:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: '/tic-tac-toe/'
   //   build: {
   //     rollupOptions: {
   //       input: {
